8363c851fb189b38f4cfe362f268d6175b144808
timakeita01-beep <tima.keita01@gmail.com>
2026-08-04 16:46:41 +0000

    app classe eleves matieres terminer

--- matieres/admin.py
+++ matieres/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from . models import *
-
-# Register your models here.
-admin.site.register(Matiere)
+from .models import Matiere
+ 
+@admin.register(Matiere)
+class MatiereAdmin(admin.ModelAdmin):
+    list_display = ("nom", "note_sur", "coefficient")
+    filter_horizontal = ("classes",)
+    search_fields = ("nom",)
+ 
--- matieres/urls.py
+++ matieres/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'matieres'
 urlpatterns = [
-    path('matieres/',views.index, name='list'),
+    path("matieres/", views.matiere_list, name="list"),
+    path("ajouter/", views.matiere_create, name="create"),
+    path("<int:pk>/", views.matiere_detail, name="detail"),
+    path("<int:pk>/modifier/", views.matiere_update, name="update"),
+    path("<int:pk>/supprimer/", views.matiere_delete, name="delete"),
 ]
+ 
